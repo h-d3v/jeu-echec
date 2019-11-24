f4f2ef56a60f1cffe29580e9b3d32b9e6c7b1236
Hedi <1756859@crosemont.qc.ca>
2019-11-23 19:53:40 -0500

    creation du projet et debut du menu d'accueil

---
+++ src/main/java/jeuEchec/ControleurBoutons.java
@@ -0,0 +1,15 @@
+package jeuEchec;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControleurBoutons {
+
+  @FXML
+  private Button bouton1;
+  
+  @FXML 
+  private void cliquer() { 
+    System.out.println("Salut le monde !"); 
+  } 
+}
---
+++ src/main/java/jeuEchec/JeuEchec.java
@@ -0,0 +1,27 @@
+package jeuEchec;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
+public class JeuEchec extends Application{
+
+  public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/jeuEchec.fxml"));
+
+    Parent root=loader.load();
+    loader.getController();
+      
+    stage.setScene(new Scene(root));
+
+    stage.show();
+
+  }
+
+    public static void main(String[] args) {
+      launch(args);
+    }
+    
+}
