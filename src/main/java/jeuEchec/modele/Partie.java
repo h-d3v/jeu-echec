3c7556fb47895dd2a2f205966a02f8ecc1055c8f
Julien Jacquard <jacquardjul@gmail.com>
2019-12-13 12:04:43 -0500

    Debut de la classe Partie Diagramme a corriger

--- src/main/java/jeuEchec/modele/Partie.java
+++ src/main/java/jeuEchec/modele/Partie.java
@@ -1,4 +1,31 @@
 package jeuEchec.modele;
 
+import com.google.common.base.Stopwatch;
+
+;
+
+
+
 public class Partie {
+
+    private int idPartie;
+    private Joueur joueurBlanc;
+    private Joueur joueurNoir;
+    private boolean partieEnCours;
+    private Stopwatch tempsPartie;
+    private Joueur tourJoueur;
+    private Joueur joueurGagant;
+    private static int compteur=1;
+
+    /**
+     * Constructeur d'une partie
+     * @param joueurBlanc
+     * @param  joueurNoir
+     */
+    Partie(Joueur joueurBlanc, Joueur joueurNoir){
+        idPartie=compteur;
+        compteur++;
+        this.joueurBlanc=joueurBlanc;
+        this.joueurNoir=joueurNoir;
+    }
 }
