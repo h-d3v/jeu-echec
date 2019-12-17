8d31a06d5d11dadf452ef419172c69b293d429a3
Hedi <1756859@crosemont.qc.ca>
2019-12-16 22:29:47 -0500

    Ajout des fichier xml, debut des controlleurs et Modele

--- src/main/java/jeuEchec/JeuEchec.java
+++ src/main/java/jeuEchec/JeuEchec.java
@@ -10,14 +10,11 @@ public class JeuEchec extends Application{
 
   public void start(Stage stage) throws Exception {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/menuPrincipal.fxml"));
-
     Parent root=loader.load();
-    loader.getController();
-      
     stage.setScene(new Scene(root));
-
+    stage.setTitle("JeuEchec");
+    ((ControleurMenuPrincipal)loader.getController()).setParent(stage);
     stage.show();
-
   }
 
     public static void main(String[] args) {
---
+++ src/main/java/jeuEchec/Modele.java
@@ -0,0 +1,15 @@
+package jeuEchec;
+
+import jeuEchec.modele.Partie;
+import jeuEchec.modele.PartieDAO;
+
+import java.util.ArrayList;
+
+public class Modele {
+    protected ArrayList<Partie> parties;
+    protected Partie partieCourrante;
+
+    public void ajouterPartie(){
+
+    }
+}
